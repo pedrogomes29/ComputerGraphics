c12022d77a25aeb09119b7fa400ca5024be90f79
Pedro Gomes <up202006322@edu.fe.up.pt>
2023-03-13 17:14:43 +0000

    TP3-2

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -19,32 +19,66 @@ export class MyPrism extends CGFobject {
     initBuffers() {
         this.vertices = [];
         this.indices = [];
+        this.normals = [];
 
-        for(let i=0;i<=this.stacks;i++){
-            const height = (1/this.stacks)*i;
-            for(let j=0;j<=this.slices;j++){
-                const ang = j*((2*Math.PI)/this.slices);
-                this.vertices.push(Math.cos(ang),-Math.sin(ang),height);
-            }
-        }
-        for(let i=1;i<=this.stacks;i++){
-            for(let j=1;j<=this.slices;j++){
-                    const upper_right = (i-1)*(this.slices+1)+j
-                    const upper_left = i*(this.slices+1)+j
-                    const bottom_right = (i-1)*(this.slices+1)+j-1
-                    const bottom_left = i*(this.slices+1)+j-1
+        const number_of_vertices = (this.stacks+1)*this.slices
 
-                    console.log(upper_right,upper_left,bottom_right,bottom_left)
 
-                    this.indices.push(bottom_right,bottom_left,upper_left)
-                    this.indices.push(bottom_right,upper_left,upper_right)
+        for(let points_offset=0; points_offset<2*number_of_vertices; points_offset+=number_of_vertices){
+            for(let i=0;i<=this.stacks;i++){
+                const height = (1/this.stacks)*i;
+                for(let j=0;j<this.slices;j++){
+                    const ang = j*((2*Math.PI)/this.slices);
+                    this.vertices.push(Math.cos(ang),Math.sin(ang),height);
+
+                    if(points_offset==0){
+                        const previous_ang = (j-1)*((2*Math.PI)/this.slices);
+                        const side_ang = (ang+previous_ang)/2
+                        this.normals.push(Math.cos(side_ang),Math.sin(side_ang),0)
+                    }
+                    else{
+                        const next_ang = (j+1)*((2*Math.PI)/this.slices);
+                        const side_ang = (ang+next_ang)/2
+                        this.normals.push(Math.cos(side_ang),Math.sin(side_ang),0)
+                    }
+                    if(i>0 && j>0){
+                        const upper_right = points_offset+(i-1)*this.slices+j
+                        const upper_left = points_offset+i*this.slices+j
+                        const bottom_right = points_offset+(i-1)*this.slices+j-1
+                        const bottom_left = points_offset+i*this.slices+j-1
+
+                        this.indices.push(upper_right,upper_left,bottom_left)
+                        this.indices.push(bottom_left,bottom_right,upper_right)            
+                    }
+                }
+                if(i>0){
+                    const last_ang = this.slices-1
+                    const upper_right = points_offset+(i-1)*this.slices
+                    const upper_left = points_offset+i*this.slices    
+                    const bottom_right = points_offset+(i-1)*this.slices + last_ang
+                    const bottom_left = points_offset+i*this.slices+ last_ang
+                
+                    this.indices.push(upper_right,upper_left,bottom_left)
+                    this.indices.push(bottom_left,bottom_right,upper_right)    
                 }
             }
-      
-        
+        }
+
         this.primitiveType = this.scene.gl.TRIANGLES;
     
         this.initGLBuffers();
       }
-      updat
+      updateBuffers(){
+        this.initBuffers();
+        this.initNormalVizBuffers();
+      }
+
+      display(){
+        this.scene.popMatrix();
+        this.scene.pushMatrix();    
+        this.scene.rotate(-Math.PI/2,1,0,0);
+        super.display();
+        this.scene.popMatrix();
+        this.scene.pushMatrix(); 
+      }
 }
--- tp3/README.md
+++ tp3/README.md
@@ -15,4 +15,14 @@
 
 ![Screenshot 2](screenshots/cg-t08g10-tp3-2.png)
 
+**Part2**
+
+- We had a bit of difficulties finding out what the correct indices were, when drawing the prism, but we managed to do it in the end.
+- When creating the normals for the prism, we had a bit of difficulties, because we had to duplicate each vertex, and then create the normals for each of the duplicated vertices.
+- It is simillar to Constant Shading, because we use the normal of one point of each side (in this case, the center of the side) as the normal for each of the 4 vertices of that side. We are basically assuming that each vertex (hence every single point of the side since the light of every single point in the side is calculated as an interpolation from the light of the vertices) has the normal of the center of the side.
+- We assumed that "vertical column" meant a column whose base is in the z=0 plane, and that the column was supposed to always have height 1, in spite of the number of stacks.
+
+![Screenshot 3](screenshots/cg-t08g10-tp3-3.png)
+
+
 
