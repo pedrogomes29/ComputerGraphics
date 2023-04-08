6742cb00a3ecb225c9315fd7293b611335b7390e
rui-exe <mrswager173@gmail.com>
2023-04-08 16:59:32 +0100

    esfera com textura v2

--- project/MySphere.js
+++ project/MySphere.js
@@ -1,4 +1,4 @@
-import { CGFobject } from "../lib/CGF.js";
+import { CGFobject} from "../lib/CGF.js";
 /**
  * MySphere
  * @constructor
@@ -21,53 +21,45 @@ export class MySphere extends CGFobject {
         this.indices = [];
         this.normals = [];
         this.texCoords = [];
-
+    
         for(let i=0;i<=this.slices;i++){
-            for(let j=0;j<this.stacks;j++){
+            for(let j=0;j<=this.stacks;j++){ // increase the number of stacks by one
                 const z = -1+2*(1/this.slices)*i;
                 const radius = Math.sqrt(1-z*z);
-                const ang = j*((2*Math.PI)/this.stacks);
-                const x = radius*Math.cos(ang);
-                const y = radius*Math.sin(ang);
-
-
+                const theta = (j / this.stacks) * 2 * Math.PI;
+                const x = radius*Math.cos(theta);
+                const y = radius*Math.sin(theta);
+    
                 this.vertices.push(x,y,z);
                 const normalizationOfNormal = Math.sqrt(radius*radius+z*z);
                 this.normals.push(x/normalizationOfNormal,y/normalizationOfNormal,z/normalizationOfNormal)
                 
-                const theta = Math.atan2(y, x)
-                const phi = Math.acos(z)
-                const u = (theta + Math.PI) / (2 * Math.PI)
-                const v = (phi+Math.PI) / Math.PI
+                const phi = (i / this.slices) * Math.PI;
+                const u = (Math.PI+theta) / (2 * Math.PI)
+                const v = (Math.PI-phi) / Math.PI
                 this.texCoords.push(u,v)
-
+    
                 if(i>0 && j>0){
-                    const upper_right = (i-1)*this.stacks+j
-                    const upper_left = i*this.stacks+j
-                    const bottom_right = (i-1)*this.stacks+j-1
-                    const bottom_left = i*this.stacks+j-1
-
+                    const upper_right = (i-1)*(this.stacks+1)+j;
+                    const upper_left = i*(this.stacks+1)+j;
+                    const bottom_right = (i-1)*(this.stacks+1)+j-1;
+                    const bottom_left = i*(this.stacks+1)+j-1;
+                    
+                    //this.indices.push(bottom_left,upper_left,upper_right)
+                    //this.indices.push(bottom_right,bottom_left,upper_right)
                     this.indices.push(upper_right,upper_left,bottom_left)
-                    this.indices.push(bottom_left,bottom_right,upper_right)            
+                    this.indices.push(bottom_left,bottom_right,upper_right)  
+
                 }
             }
-            if(i>0){
-                const last_ang = this.stacks-1
-                const upper_right = (i-1)*this.stacks
-                const upper_left = i*this.stacks    
-                const bottom_right = (i-1)*this.stacks + last_ang
-                const bottom_left = i*this.stacks+ last_ang
-            
-                this.indices.push(upper_right,upper_left,bottom_left)
-                this.indices.push(bottom_left,bottom_right,upper_right)    
-            }
         }
-
+    
         this.primitiveType = this.scene.gl.TRIANGLES;
     
         this.initGLBuffers();
-      }
-      updateBuffers(){
+    }
+
+    updateBuffers(){
         this.initBuffers();
         this.initNormalVizBuffers();
       }
@@ -76,6 +68,7 @@ export class MySphere extends CGFobject {
         this.scene.popMatrix();
         this.scene.pushMatrix();    
         this.scene.rotate(-Math.PI/2,1,0,0);
+        //this.scene.scale(200,200,200,1);
         super.display();
         this.scene.popMatrix();
         this.scene.pushMatrix(); 
