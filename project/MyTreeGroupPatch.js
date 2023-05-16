a38e3978c9d314d84adddca47ffe9aaf18da3744
Rui Pires <up202008252@edu.fe.up.pt>
2023-05-16 11:17:02 +0100

    trees done

--- project/MyTreeGroupPatch.js
+++ project/MyTreeGroupPatch.js
@@ -9,17 +9,22 @@ export class MyTreeGroupPatch {
   constructor(scene) {
     this.scene = scene;
     this.trees = [];
+    this.offsets = []; // Store the random offsets for each tree
 
-    // Create the trees
+    // Create the trees and generate random offsets
     for (let i = 0; i < 9; i++) {
       this.trees.push(new MyBillBoard(this.scene));
+
+      // Generate random offsets for each tree
+      let xOffset = Math.random() * 5 - 2.5;
+      let zOffset = Math.random() * 5 - 2.5;
+      this.offsets.push({ x: xOffset, z: zOffset });
     }
   }
 
   display() {
     this.scene.popMatrix();
     this.scene.pushMatrix();
-
 
     // Display the trees
     let increment = -30;
@@ -28,7 +33,11 @@ export class MyTreeGroupPatch {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         x += increment;
-        this.trees[i * 3 + j].display(x, 13, z);
+
+        // Retrieve the stored random offsets for the current tree
+        let offset = this.offsets[i * 3 + j];
+
+        this.trees[i * 3 + j].display(x + offset.x, 13, z + offset.z);
       }
       x = increment;
       z += increment;
